7e98ca9048cdeee5aa7cb86489d65b7042b8138d
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2025-01-11 21:11:13 +0100

    experiment with modules in nodeJS, creating Class and inheritance, but mostly event emitter, event listeners and creating servers

--- logger.js
+++ logger.js
@@ -1,20 +1,31 @@
-const url = 'http://mylogger.io/log'
+const EventEmitter = require('events')
+console.log(typeof(EventEmitter));
 
-function logMessage(message) {
-  // Send an HTTP request
-  console.log(message);
+// with the keyword "extends" the new class Logger inherits all of
+// EventEmitter base class functionality and characteristics
+class Logger extends EventEmitter {
+
+  // we lose the function word when we attach it within a class.
+  // The function becomes a method!!!
+  log(message) {
+    // Send an HTTP request
+    console.log(message);
+
+    // Raise an event
+    // by using "this", instead of creating an instance of a new EventEmitter (emitter)
+    this.emit('messageLogged', { id: 1, url: 'http://...' })
+  }
 }
 
-function addNumber(num1, num2) {
-  let sum = num1 + num2;
-  console.log(sum);
-}
+
+
 // exporting the function within an object, making it available
-
-module.exports.logMsg = logMessage;
-module.exports.add = addNumber;
-
+module.exports = Logger;
 
 // you can also rename it by:
-
 // module.exports.banana = logMsg;
+
+
+//export constant variable so that it is used from app.js
+// const url = 'http://mylogger.io/log'
+// module.exports.url = url;
