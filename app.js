0bb144ec4fbdf979f3599a78519eb041e8a92f79
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2025-01-22 22:59:54 +0100

    delete commented server practice code

--- app.js
+++ app.js
@@ -1,33 +1,3 @@
-// PRACTICING with http module
-// const http = require('http');
-
-// server inherits from net.Server which -> is an EventEmitter!
-// So it has all the functionalities of an EventEmitter
-// for example server.addListener(), server.on() or server.emit() etc...
-
-// const server = http.createServer((req, res) => {
-//   if (req.url === '/') {
-//     res.write('Hello World');
-//     res.end();
-//   }
-
-//   if (req.url === '/api/courses') {
-//     res.write(JSON.stringify([1, 2, 3]));
-//     res.end();
-//   }
-// });
-
-// // the event name is "connection" - found in Documentation
-// server.on('connection', (socket) => {
-//   console.log('New connection...');
-// });
-
-// // Give it a port
-// server.listen(3000);
-
-// console.log('Listening on port 3000...');
-
-
 const http = require('http');
 const fs = require('fs');
 const path = require('path');
