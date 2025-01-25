6243caa127427e6b5814c572b8d37413478a8f1b
Stamatis Vogiatzopoulos <stamvogiatzop@gmail.com>
2025-01-25 17:31:20 +0100

    delete app.js

--- server.js
+++ server.js
@@ -1,7 +1,31 @@
 require('dotenv').config()
-
 const apiKey = process.env.STRIPE_SECRET_KEY
+
 const stripe = require('stripe')(apiKey);
 const express = require('express');
-const app = express();
-app.use(express.static('public'));
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'GET' && req.url === '/') {
+    // Serve the homepage
+    fs.readFile('./index.html', (err, data) => {
+      if (err) {
+        res.writeHead(500, { 'Content-Type': 'text/plain' });
+        res.end('Server error');
+      } else {
+        res.writeHead(200, { 'Content-Type': 'text/html' });
+        res.end(data);
+      }
+    });
+  } else if (req.method === 'POST' && req.url === '/create-payment-intent') {
+    // Handle payment intent creation
+  } else {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.end('Not Found');
+  }
+});
+
+server.listen(3000, () => {
+  console.log('Server running on http://localhost:3000');
+});
